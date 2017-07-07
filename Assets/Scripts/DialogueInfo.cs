a0a89244cf7711c1e5c5bc37259aed83e5311fde
EQwon <bluetwintail@naver.com>
2017-07-07 13:32:24 +0900

    대화창 초안 구현

---
+++ Assets/Scripts/DialogueInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueInfo
+{
+	public Sprite speakerImage;
+	public string speakerName;
+	public string dialogueText;
+
+	public DialogueInfo(Sprite speakerImage, string speakerName, string dialogueText)
+    {
+        this.speakerImage = speakerImage;
+		this.speakerName = speakerName;
+		this.dialogueText = dialogueText;
+    }
+}
