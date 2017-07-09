a62976317e03fdc4e973f52361c35187c0364fc2
snugdc-computer <snugdc@gmail.com>
2017-07-09 14:48:55 +0900

    raycast작업중

---
+++ Assets/Scripts/Stage/RaycastTester.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RaycastTester : MonoBehaviour
+{
+
+    private Vector2 mousePos;
+    void Start()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos);
+
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, );
+        //if (hit.collider != null)
+        //{
+
+        
+    }
+}
