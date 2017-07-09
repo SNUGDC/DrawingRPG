1e153080444eb1561c468f97b134c0046d55d5ec
Jejun <alphacanis35@gmail.com>
2017-07-09 16:29:21 +0900

    raycast수정중5

--- Assets/Scripts/Stage/RaycastTester.cs
+++ Assets/Scripts/Stage/RaycastTester.cs
@@ -7,7 +7,7 @@ public class RaycastTester : MonoBehaviour
 {
 
     private Vector2 mousePos;
-    private Vector3 mousePos3D;
+
     void Start()
     {
 
@@ -19,7 +19,7 @@ public class RaycastTester : MonoBehaviour
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
        // Debug.Log(mousePos);
 
-        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, mousePos - new Vector2(transform.position.x ,transform.position.y));
+        RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos, new Vector2(transform.position.x ,transform.position.y)- mousePos);
         collisionPositions.Clear();
         if (hits.Length != 0)
         {
@@ -31,20 +31,6 @@ public class RaycastTester : MonoBehaviour
             }
         }
 
-
-        mousePos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos3D.z = 0;
-        RaycastHit[] hits3D = Physics.RaycastAll(transform.position,mousePos3D - new Vector3(transform.position.x, transform.position.y, transform.position.z));
-
-        if (hits3D.Length != 0)
-        {
-            for (int i = 0; i < hits3D.Length; i++)
-            {
-                collisionPositions.Add(hits3D[i].point);
-                Debug.Log("3d" + i.ToString() + hits3D[i].collider);
-
-            }
-        }
 
     }
 
