d6c5795da914d2cec1819f2d49d39209c701f94f
Logtic <celestialine@naver.com>
2017-07-13 18:22:53 +0900

    플레이어 hp가 0이하면 게임오버되요
    그러나 턴 수 게임오버는 아직..

--- Assets/Scripts/UIppt_Ingame/Stage/HP.cs
+++ Assets/Scripts/UIppt_Ingame/Stage/HP.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 public class HP : MonoBehaviour {
     private int maxHP;
     private int current_HP;
+
+    private float duration = 3.0f;
+    private bool is_game_over = false;
     public Player player;
     public Sprite blank_hp;
     public Sprite full_hp;
+    public GameObject Game_Over_Image;
     public Image[] player_hp_bar;
 	// Use this for initialization
 	void Start () {
@@ -16,11 +20,20 @@ public class HP : MonoBehaviour {
         current_HP = player.hp;
         check_HP();
 	}
-
+    
+    
     void Update()
     {
         current_HP = player.hp;
         check_HP();
+        if (current_HP <= 0 && is_game_over == false)
+        {
+            Game_Over_Image.GetComponent<Image>().canvasRenderer.SetAlpha(0);
+            Game_Over_Image.SetActive(true);
+            Game_Over_Image.GetComponent<Image>().CrossFadeAlpha(1, duration, true);
+            is_game_over = true;
+        }
+            
     }
 
     void check_HP()
