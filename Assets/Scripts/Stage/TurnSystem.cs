de2d7955a0d8823e4c6e333b576dbe6143854611
EQwon <bluetwintail@naver.com>
2017-07-14 12:49:50 +0900

    전투 페이즈 시작 알림

--- Assets/Scripts/Stage/TurnSystem.cs
+++ Assets/Scripts/Stage/TurnSystem.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class TurnSystem : MonoBehaviour
 {
     public Image[] MovePhaseImage;
+    public Image[] BattlePhaseImage;
     private bool startmove;
     public Player player;
 
@@ -27,6 +28,7 @@ public class TurnSystem : MonoBehaviour
         {
             yield return StartCoroutine(StartMovePhase());
             yield return StartCoroutine(RunMovePhase());
+            yield return StartCoroutine(StartBattlePhase());
             yield return StartCoroutine(RunBattlePhase());
             if (player == null)
             {
@@ -48,6 +50,14 @@ public class TurnSystem : MonoBehaviour
     private IEnumerator RunMovePhase()
     {
         yield return StartCoroutine(player.RunMovePhase());
+    }
+
+    private IEnumerator StartBattlePhase()
+    {
+        ReturnBattlePhaseImageToOrigin();
+        yield return StartCoroutine(ShowBattlePhasePanel());
+        yield return new WaitForSeconds(1.5f);
+        yield return StartCoroutine(FadeBattlePhasePanel());
     }
 
     private IEnumerator RunBattlePhase()
@@ -111,6 +121,67 @@ public class TurnSystem : MonoBehaviour
         MovePhaseImage[3].rectTransform.anchoredPosition = new Vector2(0, 1980);
 
         foreach(Image image in MovePhaseImage)
+        {
+            image.color = new Color (1, 1, 1, 0);
+        }
+    }
+
+    private IEnumerator ShowBattlePhasePanel()
+    {
+        while(true)
+        {
+            bool isShow = (BattlePhaseImage[0].color.a >= 1f);
+            Vector2[] movingVector = new Vector2[] {new Vector2(-1980,0), new Vector2(1980,0), new Vector2(0,1980), new Vector2(0,-1980)};
+
+            if(isShow == false)
+            {
+                for(int i = 0; i < movingVector.Length; i++)
+                {
+                    BattlePhaseImage[i].color += new Color(0, 0, 0, Time.deltaTime);
+                    BattlePhaseImage[i].rectTransform.anchoredPosition += movingVector[i] * Time.deltaTime;
+                }
+            }
+            else
+            {
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeBattlePhasePanel()
+    {
+        while(true)
+        {
+            bool isShow = (BattlePhaseImage[0].color.a <= 0f);
+            Vector2[] movingVector = new Vector2[] {new Vector2(-1980,0), new Vector2(1980,0), new Vector2(0,1980), new Vector2(0,-1980)};
+
+            if(isShow == false)
+            {
+                for(int i = 0; i < movingVector.Length; i++)
+                {
+                    BattlePhaseImage[i].color -= new Color(0, 0, 0, Time.deltaTime);
+                    BattlePhaseImage[i].rectTransform.anchoredPosition -= movingVector[i] * Time.deltaTime;
+                }
+            }
+            else
+            {
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
+    private void ReturnBattlePhaseImageToOrigin()
+    {
+        BattlePhaseImage[0].rectTransform.anchoredPosition = new Vector2(1980, 0);
+        BattlePhaseImage[1].rectTransform.anchoredPosition = new Vector2(-1980, 0);
+        BattlePhaseImage[2].rectTransform.anchoredPosition = new Vector2(0, -1980);
+        BattlePhaseImage[3].rectTransform.anchoredPosition = new Vector2(0, 1980);
+
+        foreach(Image image in BattlePhaseImage)
         {
             image.color = new Color (1, 1, 1, 0);
         }
