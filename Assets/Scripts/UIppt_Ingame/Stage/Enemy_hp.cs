aaafe4758e429aa5e0747892de952e768914311f
jayin301 <alswjdghk@snu.ac.kr>
2017-07-24 18:45:08 +0900

    change variable name full_hp to hpUnit, blank_hp to hpLoss

--- Assets/Scripts/UIppt_Ingame/Stage/Enemy_hp.cs
+++ Assets/Scripts/UIppt_Ingame/Stage/Enemy_hp.cs
@@ -9,15 +9,15 @@ public class Enemy_HP : MonoBehaviour {
     private int maxHP;
     private int current_HP;
     
-    public Sprite blank_hp;
-    public Sprite full_hp;
+    public Sprite hpLoss;
+    public Sprite hpUnit;
     public Image[] enemy_hp_bar;
 
     private void make_maxhp()
     {
         for (int i = 0; i < maxHP; i++)
         {
-            enemy_hp_bar[i].sprite = full_hp;
+            enemy_hp_bar[i].sprite = hpUnit;
         }
         for (int i = maxHP; i < 10; i++)
         {
@@ -31,9 +31,9 @@ public class Enemy_HP : MonoBehaviour {
         for (int i = 0; i < maxHP; i++)
         {
             if (current_HP > i)
-                enemy_hp_bar[i].sprite = full_hp;
+                enemy_hp_bar[i].sprite = hpUnit;
             else
-                enemy_hp_bar[i].sprite = blank_hp;
+                enemy_hp_bar[i].sprite = hpLoss;
         }
     }
 
