c61702f217969050fc9145b7d94f2fd1b3d85c31
Juhyeong Park <majecty@gmail.com>
2017-08-02 18:41:00 +0900

    Make drawer and drawing phase

---
+++ Assets/Scripts/Making New Game Drawing Phase/Drawer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour
+{
+    private DrawingPhase phase;
+
+    public GameObject linePrefab;
+    public float maxLineLength;
+
+    private List<GameObject> lines = new List<GameObject>();
+
+    private GameObject player;
+
+    private GameObject currentLine;
+    private Vector2 mousePos
+    {
+        get
+        {
+            return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+    private Vector2 currentLineStartPos;
+    private Vector2 currentLineEndPos;
+    private bool isInControlMode;
+
+    private List<Enemy> allEncountedEnemyList = new List<Enemy>();
+
+    public void StartDrawingPhase(DrawingPhase phase)
+    {
+        this.phase = phase;
+    }
+
+    private void Start()
+    {
+        isInControlMode = false;
+
+        allEncountedEnemyList.Clear();
+        player = transform.parent.gameObject;
+        transform.position = player.transform.position;
+    }
+
+    private void Update()
+    {
+        if (phase == null)
+        {
+            return;
+        }
+
+        if (isInControlMode == true)
+        {
+            transform.position = currentLineEndPos;
+            UpdateLinePosition(currentLine);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (phase == null)
+            return;
+
+		if (!phase.HaveDrawingTurn()) {
+			Debug.LogWarning("Not enough turn");
+			return;
+		}
+
+        Debug.Log("조작모드 시작");
+        isInControlMode = true;
+        currentLineStartPos = transform.position;
+        RaycastTester raycastTester = gameObject.GetComponent<RaycastTester>();
+        raycastTester.startPos = currentLineStartPos;
+        currentLine = Instantiate(linePrefab);
+    }
+
+    private void OnMouseUp()
+    {
+        if (phase == null)
+            return;
+        if (isInControlMode == false)
+        {
+            Debug.LogWarning("OnMouseUp But Not Control Mode");
+            return;
+        }
+
+        Debug.Log("조작모드 끝");
+        isInControlMode = false;
+
+        currentLine.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+
+        Enemy encounteredEnemy = EncountEnemy();
+
+        if (encounteredEnemy != null)
+        {
+            allEncountedEnemyList.Add(encounteredEnemy);
+        }
+        lines.Add(currentLine);
+
+		phase.UseLineCount();
+    }
+
+    private void UpdateLinePosition(GameObject line)
+    {
+        if (line == null)
+        {
+            Debug.LogError("line is null");
+            return;
+        }
+        float lineLength = Vector2.Distance(currentLineStartPos, mousePos);
+        if (EncountEnemyPosition() != null)
+        {
+            lineLength = Vector2.Distance(EncountEnemyPosition().Value, currentLineStartPos);
+            currentLineEndPos = EncountEnemyPosition().Value;
+            line.GetComponent<SpriteRenderer>().color = new Color(1, 0.2f, 0.2f, 1);
+        }
+        else if (lineLength > maxLineLength)
+        {
+            lineLength = maxLineLength;
+            currentLineEndPos = currentLineStartPos + (mousePos - currentLineStartPos).normalized * maxLineLength;
+            line.GetComponent<SpriteRenderer>().color = new Color(1, 0.2f, 0.2f, 1);
+        }
+        else
+        {
+            currentLine.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            currentLineEndPos = mousePos;
+        }
+
+        line.transform.position = (currentLineStartPos + currentLineEndPos) / 2;
+        line.transform.localScale = new Vector2(Vector2.Distance(currentLineStartPos, currentLineEndPos), 1);
+        line.transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan((currentLineEndPos.y - currentLineStartPos.y) / (currentLineEndPos.x - currentLineStartPos.x)));
+    }
+
+    private Vector2? EncountEnemyPosition()
+    {
+        RaycastTester raycastTester = gameObject.GetComponent<RaycastTester>();
+
+        return raycastTester.GetNearestEnemyPosition(ignoreList: allEncountedEnemyList);
+    }
+
+    private Enemy EncountEnemy()
+    {
+        RaycastTester raycastTester = gameObject.GetComponent<RaycastTester>();
+
+        return raycastTester.GetNearestEnemy(ignoreList: allEncountedEnemyList);
+    }
+}
---
+++ Assets/Scripts/Making New Game Drawing Phase/DrawingPhase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class DrawingPhase : MonoBehaviour
+{
+
+    public int totalLineCount = 5;
+
+    private List<Drawer> drawers = new List<Drawer>();
+    private int remainLineCount;
+
+    void FindDrawers()
+    {
+        this.drawers = FindObjectsOfType<Drawer>().ToList();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        FindDrawers();
+        foreach (Drawer drawer in drawers)
+        {
+            drawer.StartDrawingPhase(this);
+        }
+        remainLineCount = totalLineCount;
+    }
+
+    public void UseLineCount()
+    {
+        this.remainLineCount -= 1;
+    }
+
+    public bool HaveDrawingTurn()
+    {
+        return remainLineCount > 0;
+    }
+}
