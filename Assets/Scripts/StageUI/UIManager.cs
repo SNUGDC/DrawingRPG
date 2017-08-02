88753627d18b2baaa4033967ce1f2ae96dc472df
jayin301 <alswjdghk@snu.ac.kr>
2017-08-02 16:51:34 +0900

    write UIManager script

--- Assets/Scripts/StageUI/UIManager.cs
+++ Assets/Scripts/StageUI/UIManager.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour
+{
+    public GameObject lineDisplayPanel;
+    public GameObject mission;
+    public GameObject startButton;
+    public GameObject stopButton;
+    public GameObject allPlayerInformationPanel;
+    public GameObject allEnemyFieldHPPanel;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject turnDisplayPanel;
+    public GameObject movePhasePanel;
+    public GameObject battlePhasePanel;
+    public GameObject missionClearPanel;
+    public GameObject missionFailPanel;
+
+    public GameObject[] players;
+
+    IEnumerator ShowMission(float second)
+    {
+        yield return new WaitForSeconds(second);
+        InitiateUI();
+    }
+
+    private void InstantiateMission()
+    {
+        Instantiate(mission, this.transform);
+    }
+
+    private void InitiateUI()
+    {
+        Instantiate(lineDisplayPanel, this.transform);
+        Instantiate(startButton, GameObject.Find("Canvas").transform);
+        Instantiate(stopButton, GameObject.Find("Canvas").transform);
+        Instantiate(allPlayerInformationPanel, GameObject.Find("Canvas").transform);
+        Instantiate(allEnemyFieldHPPanel, GameObject.Find("Canvas").transform);
+    }
+
+    public void InstantiateStartPanel()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        Instantiate(turnDisplayPanel, this.transform);
+    }
+
+    private void Start()
+    {
+        InstantiateMission();
+        StartCoroutine(ShowMission(3.0f));
+    }
+
 }
