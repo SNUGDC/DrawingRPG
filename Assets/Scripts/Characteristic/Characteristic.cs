52b872468bcfe8d639ae2c9efea0863a7ff0515c
Logtic <celestialine@naver.com>
2017-08-16 18:52:29 +0900

    특성창만드는중

---
+++ Assets/Scripts/Characteristic/Characteristic.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Characteristic : MonoBehaviour {
+
+    public Image portrait;
+    public Text characterInfo;
+    public List<Player> playerList;
+    public List<GameObject> characteristicPanelList;
+    public int characterNum;
+    
+    public void CharacterInfoText(Player player)
+    {
+        characterInfo.text = "플레이어 이름\n" + "HP : " + player.maxHp + "/" + player.hp;
+    }
+    public void CharacterChooseButton(int num)
+    {
+        characterNum = num;
+        portrait.sprite = playerList[num].portrait;
+        CharacterInfoText(playerList[num]);
+        foreach(GameObject objects in characteristicPanelList)
+        {
+            objects.SetActive(false);
+        }
+        characteristicPanelList[num].SetActive(true);
+    }
+    public void OpenDetailCharacteristicInfo()
+    {
+        GameObject DetailPanel = characteristicPanelList[characterNum].transform.Find("DetailPanel").gameObject;
+        DetailPanel.SetActive(true);
+    }
+
+    public void UpCharacterHP()
+    {
+        Player player = playerList[characterNum];
+        player.maxHp++;
+    }
+    public void DownCharacterHP()
+    {
+        Player player = playerList[characterNum];
+        player.maxHp--;
+    }
+    
+    public void CloseDetailCharacteristicInfo()
+    {
+        GameObject DetailPanel = characteristicPanelList[characterNum].transform.Find("DetailPanel").gameObject;
+        DetailPanel.SetActive(false);
+    }
+}
