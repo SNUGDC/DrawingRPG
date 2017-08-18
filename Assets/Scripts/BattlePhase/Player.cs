cce34068f5a6f1fce80be56c28cdcf155477204b
jayin301 <alswjdghk@snu.ac.kr>
2017-08-18 18:23:13 +0900

    Skill Tree basic code

--- Assets/Scripts/BattlePhase/Player.cs
+++ Assets/Scripts/BattlePhase/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,6 +19,11 @@ public class Player : MonoBehaviour
     public Element element;
     public CharacterName characterName;
     public Sprite portrait;
+
+    private void setSkillTree()
+    {
+
+    }
 
     public void Start()
     {
---
+++ Assets/Scripts/BattlePhase/Skill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Skill : MonoBehaviour
+{
+    private List<Skill> nextSkills;
+
+    public abstract void Use(Player player);
+
+    public Skill()
+    {
+        nextSkills = new List<Skill>();
+    }
+
+    public void addNextSkill(Skill skill)
+    {
+        nextSkills.Add(skill);
+    }
+}
