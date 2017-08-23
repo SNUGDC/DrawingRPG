45536ef852dbdeb1be08e8024d34a1c2791837d9
Logtic <celestialine@naver.com>
2017-08-23 23:47:53 +0900

    stageSelect 이미지 적용

--- Assets/Scripts/StageSelect/StageButton.cs
+++ Assets/Scripts/StageSelect/StageButton.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class StageButton : MonoBehaviour
-{/*
+{
     public int stage;
+    public Image stageImage;
+    public List<Sprite> Flags;
     void Awake()
     {
         int lastClearedStage = SaveManager.LoadLastClearedStage();
-
-        if (stage <= lastClearedStage + 1)
+        
+        if (stage == lastClearedStage)
         {
+            stageImage.sprite = Flags[1];
+            GetComponent<Button>().interactable = true;
+        }
+        else if (stage < lastClearedStage)
+        {
+            stageImage.sprite = Flags[0];
             GetComponent<Button>().interactable = true;
         }
         else
         {
+            stageImage.sprite = null;
             GetComponent<Button>().interactable = false;
         }
-    }*/
+    }
 }
